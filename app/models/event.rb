6d3518f90e78c39338e8e310156d2f8dad0457d6
Cedric Le Net <lenetcedric@gmail.com>
2021-11-08 11:58:06 +0100

    validators

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,25 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :attendances
   has_many :users, through: :attendances
+  validates :start_date, presence: true
+  validate :date_is_not_passed
+  validate :duration_is_positive_and_multiple_of_five
+  validates :title, presence: true , length: {minimum: 5, maximum: 140}
+  validates :description, presence: true , length: {minimum: 20, maximum: 140}
+  validates :price, inclusion: { in: 1..1000, message: 'The price must be between 1 and 1000' }
+  validates :location, presence: true
+end
+
+def date_is_not_passed
+  if start_date < Time.now
+    errors.add(:start_date, "can't be in the past")
+  end
+end
+
+def duration_is_positive_and_multiple_of_five
+  if duration < 0
+    errors.add(:duration, "can't be negative")
+  elsif duration % 5 != 0
+    errors.add(:duration, "should be a multiple of five")
+  end
 end
