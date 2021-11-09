07af77e843f412463fb63e55746c3188e9f2b4be
Cedric Le Net <lenetcedric@gmail.com>
2021-11-09 16:29:23 +0100

    event create and event show

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,17 +1,27 @@
 class EventsController < ApplicationController
+
+  before_action :authenticate_user!, only: [:new]
+
   def index
-    @events = Event.all
+    @events = Event.all.order('updated_at DESC')
   end
 
   def new
-    @event = Event.new(user:current_user,start_date:Time.now+4000,location:"toto city")
+     @event = Event.new(user:current_user,start_date:Time.now+4000,location:"toto city")
   end
 
   def create
     event_params = params.require(:event).permit(:title,:description,:start_date,:duration,:price,:location)
-    puts event_params[:location].inspect
-    puts event_params[:"start_date(4i)"].inspect
-    puts event_params.inspect
+    event_params[:user] = current_user
+    @event = Event.new(event_params)
+    if @event.valid?
+      @event.save
+      redirect_to root_path
+    else
+      render "new"
+      puts "NOOOOOOOOOOOOOOOOOOOOOOO"
+      puts @event.errors.messages
+    end
   end
 
   def show
